3404f5b2aa2dfe48b227e997b026965378f9161a
brenob6 <brenob6@gmail.com>
2023-11-05 00:25:59 -0300

    add movement keys

--- internal/view/mainModel.go
+++ internal/view/mainModel.go
@@ -5,7 +5,7 @@ import (
 )
 
 type MainModel struct {
-	board board
+	board *board
 }
 
 func (m MainModel) Init() tea.Cmd {
@@ -17,14 +17,32 @@ func (m MainModel) View() string {
 }
 
 func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
+
+		case "h":
+			if m.board.x > 0 {
+				m.board.x--
+			}
+		case "l":
+			if m.board.x < 3 {
+				m.board.x++
+			}
+		case "j":
+			if m.board.y < 4 {
+				m.board.y++
+			}
+		case "k":
+			if m.board.y > 1 {
+				m.board.y--
+			}
 		}	
 	}
-	return m, nil
+	return m, cmd
 }
 
 func InitMainModel() MainModel {
--- internal/view/table.go
+++ internal/view/table.go
@@ -10,9 +10,11 @@ type board struct {
 	x, y int
 }
 
-func newBoard () board{
+func newBoard () *board{
 
-	var b board
+	b := new(board)
+	b.x = 0
+	b.y = 1
 
 	row := [][]string {
 		{"01", "02", "03", "04"},
@@ -21,21 +23,19 @@ func newBoard () board{
 		{"13", "14", "15", "X"},
 	}
 
-	t := table.New().
+	b.t = table.New().
 		Border(lipgloss.NormalBorder()).
 		BorderRow(true).
 		StyleFunc(func(row, col int) lipgloss.Style {
 			style := lipgloss.NewStyle().
 				Height(2).Width(4)
 
-			if col == 0 && row == 1 {
+			if col == b.x && row == b.y {
 				style = style.Foreground(lipgloss.Color("99"))
 			}
 			return style
 		}).
 		Rows(row...)
-
-	b.t = t
 
 	return b
 }
