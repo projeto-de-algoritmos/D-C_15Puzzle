5df3e5b43bb81f116b8e388ab755edfa4a9db9dd
brenob6 <brenob6@gmail.com>
2023-11-05 11:43:25 -0300

    add log file

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"15puzzle/internal/view"
 	"log"
+	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
 	mainModel := view.InitMainModel()
+
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		os.Exit(1)
+	}
+
 	p := tea.NewProgram(mainModel)
 
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
 }
